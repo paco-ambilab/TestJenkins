4287b02f35676de5b2f9876ca1bfb3e8d9fd5ee5
pacoyeung <paco.ambilab@icloud.com>
2019-08-27 18:38:29 +0800

    change fastlane config

---
+++ Pods/Target Support Files/Pods-FirstJenkins/Pods-FirstJenkins-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirstJenkins : NSObject
+@end
+@implementation PodsDummy_Pods_FirstJenkins
+@end
---
+++ Pods/Target Support Files/Pods-FirstJenkinsTests/Pods-FirstJenkinsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirstJenkinsTests : NSObject
+@end
+@implementation PodsDummy_Pods_FirstJenkinsTests
+@end
